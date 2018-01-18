b84e63b968d4cdf3a97a5bb6bdeb9367d89477fd
Rohan Goyal <rohangoyal1407@gmail.com>
2018-01-18 20:48:30 +0530

    deleted the boston_housing.py and added 2 new file predict_housing.py as well as housing.csv

--- predict_housing.py
+++ predict_housing.py
@@ -1,33 +1,34 @@
-# Import libraries necessary for this project
 import numpy as np
 import pandas as pd
 from sklearn.cross_validation import ShuffleSplit
+from sklearn.cross_validation import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import make_scorer
 
-# Load the Boston housing dataset
+
+
 data = pd.read_csv('housing.csv')
 prices = data['MEDV']
 features = data.drop('MEDV', axis = 1)
 
-# Success
-print "Boston housing dataset has {} data points with {} variables each.".format(*data.shape)
-
-# TODO: Minimum price of the data
+# Minimum price of the data
 minimum_price = np.min(prices)
 
-# TODO: Maximum price of the data
+# Maximum price of the data
 maximum_price = np.max(prices)
 
-# TODO: Mean price of the data
+# Mean price of the data
 mean_price = np.mean(prices)
 
-# TODO: Median price of the data
+# Median price of the data
 median_price = np.median(prices)
 
-# TODO: Standard deviation of prices of the data
+# Standard deviation of prices of the data
 std_price = np.std(prices)
 
 # Show the calculated statistics
-print "Statistics for Boston housing dataset:\n"
+print "Statistics for housing dataset:\n"
 print "Minimum price: ${:,.2f}".format(minimum_price)
 print "Maximum price: ${:,.2f}".format(maximum_price)
 print "Mean price: ${:,.2f}".format(mean_price)
@@ -38,12 +39,12 @@ print "Standard deviation of prices: ${:,.2f}".format(std_price)
 from sklearn.metrics import r2_score
 
 def performance_metric(y_true, y_predict):
-    """ Calculates and returns the performance score between
+    """ Calculates and returns the performance score between 
         true and predicted values based on the metric chosen. """
-
+    
     #Calculating the performance score between 'y_true' and 'y_predict'
     score = r2_score(y_true, y_predict)
-
+    
     # Return the score
     return score
 
@@ -51,59 +52,45 @@ def performance_metric(y_true, y_predict):
 score = performance_metric([3, -0.5, 2, 7, 4.2], [2.5, 0.0, 2.1, 7.8, 5.3])
 print "Model has a coefficient of determination, R^2, of {:.3f}.".format(score)
 
-# TODO: Import 'train_test_split'
-from sklearn.cross_validation import train_test_split
 
-# TODO: Shuffle and split the data into training and testing subsets
+
+# Shuffle and split the data into training and testing subsets
 X_train, X_test, y_train, y_test = train_test_split(features,prices, test_size=0.2, train_size=0.8, random_state=20)
-# Success
 print "Training and testing split was successful."
 
-
-# TODO: Import 'make_scorer', 'DecisionTreeRegressor', and 'GridSearchCV'
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.grid_search import GridSearchCV
-from sklearn.metrics import make_scorer
-
 def fit_model(X, y):
-    """ Performs grid search over the 'max_depth' parameter for a
+    """ Performs grid search over the 'max_depth' parameter for a 
         decision tree regressor trained on the input data [X, y]. """
-
+    
     # Create cross-validation sets from the training data
-    # sklearn version 0.18: ShuffleSplit(n_splits=10, test_size=0.1, train_size=None, random_state=None)
-    # sklearn versiin 0.17: ShuffleSplit(n, n_iter=10, test_size=0.1, train_size=None, random_state=None)
     cv_sets = ShuffleSplit(X.shape[0], n_iter = 10, test_size = 0.20, random_state = 0)
 
-    # TODO: Create a decision tree regressor object
+    # Create a decision tree regressor object
     regressor = DecisionTreeRegressor(random_state=0)
 
-    # TODO: Create a dictionary for the parameter 'max_depth' with a range from 1 to 10
+    # Create a dictionary for the parameter 'max_depth' with a range from 1 to 10
     params = dict(max_depth=range(1, 11))
 
-    # TODO: Transform 'performance_metric' into a scoring function using 'make_scorer'
+    # Transform 'performance_metric' into a scoring function using 'make_scorer' 
     scoring_fnc = make_scorer(performance_metric)
 
-    # TODO: Create the grid search cv object --> GridSearchCV()
-    # Make sure to include the right parameters in the object:
-    # (estimator, param_grid, scoring, cv) which have values 'regressor', 'params', 'scoring_fnc', and 'cv_sets' respectively.
+    # Create the grid search cv object --> GridSearchCV
     grid = GridSearchCV(regressor, params, cv=cv_sets, scoring=scoring_fnc)
 
     # Fit the grid search object to the data to compute the optimal model
     grid = grid.fit(X, y)
 
-    # Return the optimal model after fitting the data
     return grid.best_estimator_
 
-# Fit the training data to the model using grid search
+
 reg = fit_model(X_train, y_train)
 
-# Produce the value for 'max_depth'
-print "Parameter 'max_depth' is {} for the optimal model.".format(reg.get_params()['max_depth'])
+data1 = input('Enter number of rooms: ')
+data2 = input('Enter stat of neighbour: ')
+data3 = input('Enter student teacher ratio: ')
 
-# Produce a matrix for client data
-client_data = [[5, 17, 15], # Client 1
-               [4, 32, 22], # Client 2
-               [8, 3, 12]]  # Client 3
+
+client_data = [[data1, data2, data3]] # Client 1[4, 32, 22], # Client 2[8, 3, 12]]  # Client 3
 
 # Show predictions
 for i, price in enumerate(reg.predict(client_data)):
