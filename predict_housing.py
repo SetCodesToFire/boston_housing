72987034b72f6307ea0d2af273f4fdb96d707f05
Rohan Goyal <rohangoyal1407@gmail.com>
2018-01-18 21:27:44 +0530

    Added few bug fixes in .py file

--- predict_housing.py
+++ predict_housing.py
@@ -28,40 +28,40 @@ median_price = np.median(prices)
 std_price = np.std(prices)
 
 # Show the calculated statistics
-print "Statistics for housing dataset:\n"
-print "Minimum price: ${:,.2f}".format(minimum_price)
-print "Maximum price: ${:,.2f}".format(maximum_price)
-print "Mean price: ${:,.2f}".format(mean_price)
-print "Median price ${:,.2f}".format(median_price)
-print "Standard deviation of prices: ${:,.2f}".format(std_price)
+print("Statistics for housing dataset:\n")
+print("Minimum price: ${:,.2f}".format(minimum_price))
+print("Maximum price: ${:,.2f}".format(maximum_price))
+print("Mean price: ${:,.2f}".format(mean_price))
+print("Median price ${:,.2f}".format(median_price))
+print("Standard deviation of prices: ${:,.2f}".format(std_price))
 
 #Importing r2_score from sklearn.metrics
 from sklearn.metrics import r2_score
 
 def performance_metric(y_true, y_predict):
-    """ Calculates and returns the performance score between 
+    """ Calculates and returns the performance score between
         true and predicted values based on the metric chosen. """
-    
+
     #Calculating the performance score between 'y_true' and 'y_predict'
     score = r2_score(y_true, y_predict)
-    
+
     # Return the score
     return score
 
 # Calculate the performance of this model
 score = performance_metric([3, -0.5, 2, 7, 4.2], [2.5, 0.0, 2.1, 7.8, 5.3])
-print "Model has a coefficient of determination, R^2, of {:.3f}.".format(score)
+print("Model has a coefficient of determination, R^2, of {:.3f}.".format(score))
 
 
 
 # Shuffle and split the data into training and testing subsets
 X_train, X_test, y_train, y_test = train_test_split(features,prices, test_size=0.2, train_size=0.8, random_state=20)
-print "Training and testing split was successful."
+print("Training and testing split was successful.")
 
 def fit_model(X, y):
-    """ Performs grid search over the 'max_depth' parameter for a 
+    """ Performs grid search over the 'max_depth' parameter for a
         decision tree regressor trained on the input data [X, y]. """
-    
+
     # Create cross-validation sets from the training data
     cv_sets = ShuffleSplit(X.shape[0], n_iter = 10, test_size = 0.20, random_state = 0)
 
@@ -71,7 +71,7 @@ def fit_model(X, y):
     # Create a dictionary for the parameter 'max_depth' with a range from 1 to 10
     params = dict(max_depth=range(1, 11))
 
-    # Transform 'performance_metric' into a scoring function using 'make_scorer' 
+    # Transform 'performance_metric' into a scoring function using 'make_scorer'
     scoring_fnc = make_scorer(performance_metric)
 
     # Create the grid search cv object --> GridSearchCV
@@ -94,4 +94,4 @@ client_data = [[data1, data2, data3]] # Client 1[4, 32, 22], # Client 2[8, 3, 12
 
 # Show predictions
 for i, price in enumerate(reg.predict(client_data)):
-    print "Predicted selling price for Client {}'s home: ${:,.2f}".format(i+1, price)
+    print("Predicted selling price for Client {}'s home: ${:,.2f}".format(i+1, price))
